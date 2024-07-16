5e11d1807365405b30535389b63a8f1f4a51dc6d
Dercraker <antoine.capitain@gmail.com>
2024-07-16 11:58:01 +0200

    Fix pr
    - hero Header remove js
    - page faq remove useless 'use client'

--- app/(landing-layout)/_component/hero/heroHeader.tsx
+++ app/(landing-layout)/_component/hero/heroHeader.tsx
@@ -1,36 +1,18 @@
-'use client';
-
-import { Button, Container, Text, Title, Overlay, Flex } from '@mantine/core';
-import { useRouter } from 'next/navigation';
 import { Video } from '@/components/video/video';
 import { LINKS } from '@/utils/NavigationLinks';
+import { Button, Container, Flex, Overlay, Text, Title } from '@mantine/core';
+import Link from 'next/link';
 
 export const HeroImageBackground: React.FC = () => {
-  const router = useRouter();
-
-  const handleButtonClick = () => {
-    router.push(LINKS.Dashboard.Trips.href);
-  };
-
   return (
-    <Video source='https://videos.pexels.com/video-files/5379990/5379990-uhd_2560_1440_24fps.mp4'>
+    <Video source="https://videos.pexels.com/video-files/5379990/5379990-uhd_2560_1440_24fps.mp4">
       <Overlay color="#000" opacity={0.65} zIndex={1} />
       <Container style={{ zIndex: 2 }}>
-        <Title
-          order={1}
-          ta="center"
-          c="white"
-          fw={900}
-          lts={2}
-        >
+        <Title order={1} ta="center" c="white" fw={900} lts={2}>
           Create your dream trip
         </Title>
         <Container size={640}>
-          <Text
-            size="xl"
-            ta="center"
-            c="white"
-          >
+          <Text size="xl" ta="center" c="white">
             Explore, plan, and book your next adventure
           </Text>
         </Container>
@@ -39,7 +21,8 @@ export const HeroImageBackground: React.FC = () => {
             variant="outline"
             color="white"
             size="xl"
-            onClick={handleButtonClick}
+            component={Link}
+            href={LINKS.Dashboard.Trips.href}
           >
             Start
           </Button>
